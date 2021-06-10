154316a6db1a63924104b9b56b1c9ca81d0a0e70
DreamEcho100 <maze6572198@gmail.com>
2021-06-11 01:52:44 +0200

    Update next.config.js

--- components/Home/Hero/Hero.jsx
+++ components/Home/Hero/Hero.jsx
@@ -5,7 +5,7 @@ import classes from './Hero.module.css';
 const Hero = () => {
 	return (
 		<section className={classes.hero}>
-			<h1>Home Page</h1>
+			<h1>Home Page Here {':)'}</h1>
 		</section>
 	);
 };
