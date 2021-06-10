de6233768e782b8bb0d8e05e22044e350c14fcc2
DreamEcho100 <maze6572198@gmail.com>
2021-06-11 01:35:28 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -37,9 +37,9 @@ module.exports = (phase) => {
 			// MONGODB_PASSWORD: process.env.MONGODB_PRODUCTION_PASSWORD,
 			// MONGODB_CLUSTERNAME: process.env.MONGODB_PRODUCTION_CLUSTERNAME,
 			// MONGODB_MAZENEXTBLOG_DATABASE: process.env.MONGODB_PRODUCTION_DATABASE,
-			FRONT_END_ROOT_URL: 'https://brodevlog-4rrn1dv2q-dreamecho100.vercel.app', // baseUrl, // process.env.PRODUCTION_FRONT_END_ROOT_URL,
-			BACK_END_ROOT_URL: 'https://brodevlog-4rrn1dv2q-dreamecho100.vercel.app', // baseUrl, // process.env.PRODUCTION_BACK_END_ROOT_URL,
-			FRONT_END_DOMAIN: 'https://brodevlog-4rrn1dv2q-dreamecho100.vercel.app', // baseUrl, // process.env.PRODUCTION_FRONT_END_DOMAIN,
+			FRONT_END_ROOT_URL: 'https://brodevlog-dreamecho100.vercel.app', // baseUrl, // process.env.PRODUCTION_FRONT_END_ROOT_URL,
+			BACK_END_ROOT_URL: 'https://brodevlog-dreamecho100.vercel.app', // baseUrl, // process.env.PRODUCTION_BACK_END_ROOT_URL,
+			FRONT_END_DOMAIN: 'https://brodevlog-dreamecho100.vercel.app', // baseUrl, // process.env.PRODUCTION_FRONT_END_DOMAIN,
 			PG_CONNECTION_STRING: process.env.PRODUCTION_PG_CONNECTION_STRING,
 		};
 	}
