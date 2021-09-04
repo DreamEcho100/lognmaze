72b2427558bc3b73c52784b12bf40891267af4ed
DreamEcho100 <maze6572198@gmail.com>
2021-09-04 06:05:02 +0200

    - Minor Update.

--- pages/_app.js
+++ pages/_app.js
@@ -125,7 +125,7 @@ const MyApp = ({ Component, pageProps }) => {
 				<link
 					href='https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,700;0,900;1,400;1,700;1,900&display=swap'
 					rel='stylesheet'
-					rel='preload'
+					// rel='preload'
 				/>
 			</Head>
 			<Layout isAuthenticated={pageProps.isAuthenticated}>
