638a8f16b3cc7e0204853e6594980c62f1199e61
DreamEcho100 <maze6572198@gmail.com>
2021-09-07 22:16:23 +0200

    - Minor Update.

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 // import App from 'next/app';
 import Head from 'next/head';
 import { config, library } from '@fortawesome/fontawesome-svg-core';
@@ -72,49 +73,50 @@ library.add(
 import '@styles/_globals.css';
 
 import Layout from '@components/Layout/Layout';
-import { useEffect } from 'react';
 
 const MyApp = ({ Component, pageProps }) => {
-	// // <!-- Yandex.Metrika counter -->
-	// useEffect(() => {
-	// 	(function (m, e, t, r, i, k, a) {
-	// 		m[i] =
-	// 			m[i] ||
-	// 			function () {
-	// 				(m[i].a = m[i].a || []).push(arguments);
-	// 			};
-	// 		m[i].l = 1 * new Date();
-	// 		(k = e.createElement(t)),
-	// 			(a = e.getElementsByTagName(t)[0]),
-	// 			(k.async = 1),
-	// 			(k.src = r),
-	// 			a.parentNode.insertBefore(k, a);
-	// 	})(window, document, 'script', 'https://mc.yandex.ru/metrika/tag.js', 'ym');
+	useEffect(() => {
+		// // <!-- Yandex.Metrika counter -->
+		// 	(function (m, e, t, r, i, k, a) {
+		// 		m[i] =
+		// 			m[i] ||
+		// 			function () {
+		// 				(m[i].a = m[i].a || []).push(arguments);
+		// 			};
+		// 		m[i].l = 1 * new Date();
+		// 		(k = e.createElement(t)),
+		// 			(a = e.getElementsByTagName(t)[0]),
+		// 			(k.async = 1),
+		// 			(k.src = r),
+		// 			a.parentNode.insertBefore(k, a);
+		// 	})(window, document, 'script', 'https://mc.yandex.ru/metrika/tag.js', 'ym');
 
-	// 	ym(85004128, 'init', {
-	// 		clickmap: true,
-	// 		trackLinks: true,
-	// 		accurateTrackBounce: true,
-	// 		webvisor: true,
-	// 	});
-	// 	// <!-- Yandex.Metrika counter -->
-		document.querySelector('head').innerHTML = document.querySelector('head').innerHTML + `
-			<!-- Yandex.Metrika counter -->
-			<script type="text/javascript" >
-				(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-				m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-				(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+		// 	ym(85004128, 'init', {
+		// 		clickmap: true,
+		// 		trackLinks: true,
+		// 		accurateTrackBounce: true,
+		// 		webvisor: true,
+		// 	});
+		// 	// <!-- Yandex.Metrika counter -->
+		document.querySelector('head').innerHTML =
+			document.querySelector('head').innerHTML +
+			`
+				<!-- Yandex.Metrika counter -->
+				<script type="text/javascript" >
+					(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+					m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+					(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-				ym(85004128, "init", {
-							clickmap:true,
-							trackLinks:true,
-							accurateTrackBounce:true,
-							webvisor:true
-				});
-			</script>
-			<noscript><div><img src="https://mc.yandex.ru/watch/85004128" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
-			<!-- /Yandex.Metrika counter -->
-		`
+					ym(85004128, "init", {
+								clickmap:true,
+								trackLinks:true,
+								accurateTrackBounce:true,
+								webvisor:true
+					});
+				</script>
+				<noscript><div><img src="https://mc.yandex.ru/watch/85004128" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+				<!-- /Yandex.Metrika counter -->
+			`;
 	}, []);
 
 	return (
