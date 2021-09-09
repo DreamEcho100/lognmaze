baff787a487d9cecd9fd25e8f4eb3eca4031bf8c
DreamEcho100 <maze6572198@gmail.com>
2021-09-09 23:03:22 +0200

    - Adding some codr refactoring

--- components/UI/V1/News/Action/Article/Article.jsx
+++ components/UI/V1/News/Action/Article/Article.jsx
@@ -86,13 +86,13 @@ const Article = ({
 
 		if (values.image_alt.trim().replace(/\s{2,}/g, '').length < 3)
 			fieldsCheck.push('Image title is less than 3 characters.');
-		else if (values.image_alt.trim().replace(/\s{2,}/g, '').length > 10)
-			fieldsCheck.push('Image title is more than 100 characters.');
+		else if (values.image_alt.trim().replace(/\s{2,}/g, '').length > 150)
+			fieldsCheck.push('Image title is more than 150 characters.');
 
 		if (values.image_src.replace(/\s/g, '').length === 0)
 			fieldsCheck.push('There is no image source.');
 		if (!values.image_src.replace(/\s/g, '').startsWith('https'))
-			fieldsCheck.push("Image source doesn't start with https.");
+			fieldsCheck.push("Image source doesn't start with (https).");
 
 		if (values.description.trim().replace(/\s{2,}/g, '').length < 25)
 			fieldsCheck.push('Description is less than 25 characters.');
--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -3,48 +3,55 @@
 // https://cheatcode.co/tutorials/how-to-generate-a-dynamic-sitemap-with-next-js
 
 import { getAllArticlesSlugs, getAllUsersNameId } from '@lib/v1/pg';
-const { SitemapStream, streamToPromise } = require('sitemap');
-const { Readable } = require('stream');
+import { SitemapStream, streamToPromise } from 'sitemap';
+import { Readable } from 'stream';
 
 const Sitemap = () => {};
 
 export const getServerSideProps = async ({ res, req }) => {
 	// An array with your links
-	const links = [];
-
-	try {
-		await getAllArticlesSlugs().then((articles = []) => {
-			articles.map((article) => {
-				links.push({
-					url: `/article/${article.slug}`,
-					changefreq: 'daily',
-					priority: 0.9,
-				});
-			});
-		});
-
-		await getAllUsersNameId().then((profiles = []) => {
-			profiles.map((profile) => {
-				links.push({
-					url: `/profile/${profile.user_name_id}`,
-					changefreq: 'daily',
-					priority: 0.9,
-				});
-			});
-		});
-	} catch (error) {
-		console.error(`Error, ${error.message}`);
-	}
-
-	// Add other pages
-	const pages = ['/auth'];
-	pages.map((url) => {
-		links.push({
+	const links = [
+		{
 			url,
-			changefreq: 'daily',
-			priority: 0.9,
+			lastmod: new Date().toISOString(),
+			changefreq: 'monthly',
+			priority: 1,
+		},
+	];
+
+	await getAllArticlesSlugs().then((articles = []) => {
+		articles.map((article) => {
+			links.push({
+				url: `/article/${article.slug}`,
+				lastmod: new Date().toISOString(),
+				changefreq: 'weekly',
+				priority: 0.9,
+			});
 		});
 	});
+
+	await getAllUsersNameId().then((profiles = []) => {
+		profiles.map((profile) => {
+			links.push({
+				url: `/profile/${profile.user_name_id}`,
+				lastmod: new Date().toISOString(),
+				changefreq: 'weekly',
+				priority: 0.9,
+			});
+		});
+	});
+
+	// Add static pages
+	// const pages = ['/auth'];
+
+	// pages.map((url) => {
+	// 	links.push({
+	// 		url,
+	// 		lastmod: new Date().toISOString(),
+	// 		changefreq: 'daily',
+	// 		priority: 0.9,
+	// 	});
+	// });
 
 	// Create a stream to write to
 	const stream = new SitemapStream({
@@ -53,7 +60,7 @@ export const getServerSideProps = async ({ res, req }) => {
 	});
 
 	res.writeHead(200, {
-		'Content-Type': 'application/xml',
+		'Content-Type': 'text/xml',
 	});
 
 	const xmlString = await streamToPromise(
