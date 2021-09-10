bc715e5a311a3153479696b64a5ebedca7ca1353
DreamEcho100 <maze6572198@gmail.com>
2021-09-10 10:05:59 +0200

    - Adding some styles. - Fixing minor bugs.

--- components/UI/V1/Format/Md/SyntaxHighlighter/index.jsx
+++ components/UI/V1/Format/Md/SyntaxHighlighter/index.jsx
@@ -1,9 +1,8 @@
 import { PrismLight as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { vscDarkPlus } from '@/node_modules/react-syntax-highlighter/dist/cjs/styles/prism';
+import { vscDarkPlus } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 
 const Component = (props) => {
 	return <SyntaxHighlighter style={vscDarkPlus} {...props} />;
 };
 
 export default Component;
- 
