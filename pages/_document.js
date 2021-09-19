24cfa15350ab05595b92677801e3568a946dde9b
DreamEcho100 <maze6572198@gmail.com>
2021-09-19 11:03:17 +0200

    - Minor changes.

--- pages/_document.js
+++ pages/_document.js
@@ -107,7 +107,6 @@ class MyDocument extends Document {
 					{/* <!-- Google Tag Manager (noscript) --> */}
 					<noscript>
 						<iframe
-							rel='preconnect'
 							src='https://www.googletagmanager.com/ns.html?id=GTM-N45HP9S'
 							height='0'
 							width='0'
