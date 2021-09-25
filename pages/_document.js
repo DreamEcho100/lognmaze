a8c4b7dcb7119a6dcaa891e868e9ba6d3e8b4f98
DreamEcho100 <maze6572198@gmail.com>
2021-09-25 19:33:30 +0200

    - Adding AdSense script tag to the head tag

--- pages/_document.js
+++ pages/_document.js
@@ -23,7 +23,7 @@ class MyDocument extends Document {
 						// rel='preload'
 					/>
 					<script
-						data-ad-client='ca-pub-8030984398568253'
+						dataAdClient='ca-pub-8030984398568253'
 						async
 						src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js'
 					></script>
