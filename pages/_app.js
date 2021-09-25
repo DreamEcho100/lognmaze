2f914b97a99d12f238d5e073dbf64ecb88d2172d
DreamEcho100 <maze6572198@gmail.com>
2021-09-25 20:20:36 +0200

    - Adding AdSense script tag to the head tag

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import Script from 'next/script';
 import { useRouter } from 'next/router';
-import * as gtag from '@lib/gtag';
+import * as gtag from '@lib/v1/gtag';
 
 import Head from 'next/head';
 // import { YMInitializer } from 'react-yandex-metrika';
