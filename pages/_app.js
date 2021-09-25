bdda39c0652af9f4deeaaa8bf674ae199e27db84
DreamEcho100 <maze6572198@gmail.com>
2021-09-25 20:17:46 +0200

    - Adding AdSense script tag to the head tag

--- pages/_app.js
+++ pages/_app.js
@@ -140,9 +140,15 @@ const MyApp = ({ Component, pageProps }) => {
 				</title>
 			</Head>
 			<script
+				id='ad-client-script'
 				data-ad-client='ca-pub-8030984398568253'
 				async
 				src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js'
+				dangerouslySetInnerHTML={{
+					__html: `
+					const ad-client document
+					`,
+				}}
 			></script>
 			<Script
 				strategy='afterInteractive'
