cdb1e4d3000dd597fe5ea17c354312f80827438c
DreamEcho100 <maze6572198@gmail.com>
2021-09-27 18:40:17 +0200

    - Minor update.

--- components/Layout/MainIntro/index.jsx
+++ components/Layout/MainIntro/index.jsx
@@ -9,73 +9,13 @@ import LogNMazeSignature from '@svg/LogNMazeSignature';
 const MainIntro = () => {
 	const introRef = useRef();
 
-	// const { state: userState } = useContext(UserContext);
-
 	const [animationEnd, setAnimationEnd] = useState(false);
-	// const [forceAnimationEnd, setForceAnimationEnd] = useState(false);
-	const [introHorizontal, setIntroHorizontal] = useState(
-		true // typeof window !== 'undefined' && innerWidth > innerHeight
-	);
-
-	useEffect(() => {
-		const handleResize = (element) => {
-			if (!element?.offsetWidth) return;
-			// {
-			// 	if (typeof window !== 'undefined' && innerWidth > innerHeight) {
-			// 		if (!introHorizontal) return setIntroHorizontal(true);
-			// 	}
-
-			// 	if (introHorizontal) setIntroHorizontal(false);
-
-			// 	return;
-			// }
-
-			const width = element.offsetWidth;
-			const height = element.offsetHeight;
-
-			// console.log('width', width);
-			// console.log('height', height);
-
-			if (width > height) {
-				if (!introHorizontal) return setIntroHorizontal(true);
-			} else {
-				if (introHorizontal) setIntroHorizontal(false);
-			}
-		};
-
-		handleResize(introRef.current);
-		const onResize = (event) => handleResize(introRef.current);
-
-		window.addEventListener('resize', onResize);
-
-		return () => window.removeEventListener('resize', onResize);
-	}, [introRef.current]);
 
 	// console.log('introHorizontal', introHorizontal);
 
 	return (
-		<section
-			ref={introRef}
-			className={`
-			${!introHorizontal ? classes['setVertical'] : ''} ${classes['main-intro']}
-			`}
-		>
+		<section ref={introRef} className={classes['main-intro']}>
 			<div className={classes.container}>
-				{/* <div className={classes['close-main-intro-button-container']}>
-					<button
-						title='Forcibly close the main intro'
-						className={
-							forceAnimationEnd ||
-							(!userState.isVerifyingUserLoading && animationEnd)
-								? classes.disappear
-								: ''
-						}
-						onClick={() => setForceAnimationEnd((prev) => !prev)}
-					>
-						<span></span>
-						<span></span>
-					</button>
-				</div> */}
 				<div className={classes['svg-container']}>
 					<LogNMazeSignature setAnimationEnd={setAnimationEnd} />
 				</div>
