7ca51da997b8768a72d81147c650ce4ea0b6c297
DreamEcho100 <maze6572198@gmail.com>
2021-10-16 09:50:17 +0200

    - Refactoring&optimizing code test 4.

--- components/UI/V1/NewsV2/Item/index.jsx
+++ components/UI/V1/NewsV2/Item/index.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useMemo, useState } from 'react';
+import { memo, useContext, useEffect, useMemo, useState } from 'react';
 import dynamic from 'next/dynamic';
 import NewsContext from '@store/NewsContext';
 import {
@@ -17,122 +17,131 @@ const DynamicNewsItemModal = dynamic(() => import('../NewsItemModal'), {
 import Details from './Details';
 import Header from './Header';
 
-const NewsItem = ({
-	defaultClasses = 'container',
-	extraClasses = '',
-	className = '',
-	detailsType = 'description',
-	newsItemData = {},
-	isLoadingSkeleton,
-	// newsFetchRouteQuery,
-	loadingUserVote,
-	modalOnClick,
-	hideFooterSettings,
-	modal = {},
-	userCtx = {},
-	newsCtx = {},
-}) => {
-	const userState = useMemo(() => userCtx.userState, [userCtx.userState]);
-	const userExist = useMemo(() => userCtx.userExist, [userCtx.userExist]);
-	const { state, dispatch } = useMemo(
-		() => ({ state: newsCtx.state, dispatch: newsCtx.dispatch }),
-		[newsCtx.state, newsCtx.dispatch]
-	);
+const NewsItem = memo(
+	({
+		defaultClasses = 'container',
+		extraClasses = '',
+		className = '',
+		detailsType = 'description',
+		newsItemData = {},
+		isLoadingSkeleton,
+		// newsFetchRouteQuery,
+		loadingUserVote,
+		modalOnClick,
+		hideFooterSettings,
+		// modal = {},
+		// userCtx = {},
+		// newsCtx = {},
+	}) => {
+		// const userState = useMemo(() => userCtx.userState, [userCtx.userState]);
+		// const userExist = useMemo(() => userCtx.userExist, [userCtx.userExist]);
+		// const { state, dispatch } = useMemo(
+		// 	() => ({ state: newsCtx.state, dispatch: newsCtx.dispatch }),
+		// 	[newsCtx.state, newsCtx.dispatch]
+		// );
 
-	const showModal = useMemo(() => modal.showModal, [modal.showModal]);
-	const setShowModal = modal.setShowModal;
+		// const showModal = useMemo(() => modal.showModal, [modal.showModal]);
+		// const setShowModal = modal.setShowModal;
+		const { state: userState } = useContext(UserContext);
+		const { userExist } = useContext(UserExistContext);
+		const { state, dispatch } = useContext(NewsContext);
 
-	const [isLoadingUserVote, setIsLoadingUserVote] = useState(!!loadingUserVote);
+		const [showModal, setShowModal] = useState(false);
 
-	const allClasses = useMemo(
-		() =>
-			handleAllClasses({
-				classes,
-				defaultClasses,
-				extraClasses,
-				className,
-			}),
-		[defaultClasses, extraClasses, className]
-	);
+		const [isLoadingUserVote, setIsLoadingUserVote] = useState(
+			!!loadingUserVote
+		);
 
-	const articleProps = useMemo(() => {
-		const obj = {
-			className: allClasses,
-		};
+		const allClasses = useMemo(
+			() =>
+				handleAllClasses({
+					classes,
+					defaultClasses,
+					extraClasses,
+					className,
+				}),
+			[defaultClasses, extraClasses, className]
+		);
 
-		if (newsItemData?.type === 'article')
-			obj.lang = `${newsItemData.iso_language}-${newsItemData.iso_country}`;
+		const articleProps = useMemo(() => {
+			const obj = {
+				className: allClasses,
+			};
 
-		return obj;
-	}, [newsItemData.type]);
+			if (newsItemData?.type === 'article')
+				obj.lang = `${newsItemData.iso_language}-${newsItemData.iso_country}`;
 
-	useEffect(() => {
-		if (showModal && !newsItemData?.content && newsItemData?.news_id) {
-			handleLoadingNewsItemContent({
-				dispatch,
-				news_id: newsItemData.news_id,
-			});
-		}
-	}, [showModal]);
+			return obj;
+		}, [newsItemData.type]);
 
-	useEffect(() => {
-		if (
-			isLoadingUserVote &&
-			userExist &&
-			newsItemData?.news_id &&
-			(parseInt(newsItemData.up_votes_counter) !== 0 ||
-				parseInt(newsItemData.down_votes_counter) !== 0)
-		) {
-			if (!isLoadingUserVote) setIsLoadingUserVote(true);
-			HandleLoadingUserVote({
-				dispatch,
-				news_id: newsItemData.news_id,
-				user: userState.user,
-				state,
-			});
-			if (isLoadingUserVote) setIsLoadingUserVote(false);
-		}
-	}, [isLoadingUserVote, userExist]);
+		useEffect(() => {
+			if (showModal && !newsItemData?.content && newsItemData?.news_id) {
+				handleLoadingNewsItemContent({
+					dispatch,
+					news_id: newsItemData.news_id,
+				});
+			}
+		}, [showModal]);
 
-	return (
-		<section {...articleProps}>
-			<Header
-				newsItemData={newsItemData}
-				detailsType={detailsType}
-				isLoadingSkeleton={isLoadingSkeleton}
-				setShowModal={setShowModal}
-				// setIsLoadingContent={setIsLoadingContent}
-				// isLoadingContent={isLoadingContent}
-				// hideHeaderSettings={hideHeaderSettings}
-			/>
-			<Details
-				detailsType={detailsType}
-				newsItemType={newsItemData.type}
-				details={
-					detailsType === 'description' && newsItemData.type === 'article'
-						? newsItemData.description
-						: newsItemData.content
-				}
-				newsItemId={newsItemData.news_id}
-				isLoadingSkeleton={isLoadingSkeleton}
-				setShowModal={setShowModal}
-				setShowModal={setShowModal}
-				// isLoadingContent={isLoadingContent}
-			/>
-			<footer></footer>
-			{modalOnClick && (
-				<DynamicNewsItemModal
-					showModal={showModal}
-					setShowModal={setShowModal}
-					articleProps={articleProps}
+		useEffect(() => {
+			if (
+				isLoadingUserVote &&
+				userExist &&
+				newsItemData?.news_id &&
+				(parseInt(newsItemData.up_votes_counter) !== 0 ||
+					parseInt(newsItemData.down_votes_counter) !== 0)
+			) {
+				if (!isLoadingUserVote) setIsLoadingUserVote(true);
+				HandleLoadingUserVote({
+					dispatch,
+					news_id: newsItemData.news_id,
+					user: userState.user,
+					state,
+				});
+				if (isLoadingUserVote) setIsLoadingUserVote(false);
+			}
+		}, [isLoadingUserVote, userExist]);
+
+		return (
+			<section {...articleProps}>
+				<Header
 					newsItemData={newsItemData}
-					hideFooterSettings={hideFooterSettings}
-					detailsType='content'
+					detailsType={detailsType}
+					isLoadingSkeleton={isLoadingSkeleton}
+					setShowModal={setShowModal}
+					// setIsLoadingContent={setIsLoadingContent}
+					// isLoadingContent={isLoadingContent}
+					// hideHeaderSettings={hideHeaderSettings}
 				/>
-			)}
-		</section>
-	);
-};
+				<Details
+					detailsType={detailsType}
+					newsItemType={newsItemData.type}
+					details={
+						detailsType === 'description' && newsItemData.type === 'article'
+							? newsItemData.description
+							: newsItemData.content
+					}
+					newsItemId={newsItemData.news_id}
+					isLoadingSkeleton={isLoadingSkeleton}
+					setShowModal={setShowModal}
+					setShowModal={setShowModal}
+					// isLoadingContent={isLoadingContent}
+				/>
+				<footer></footer>
+				{modalOnClick && (
+					<DynamicNewsItemModal
+						showModal={showModal}
+						setShowModal={setShowModal}
+						articleProps={articleProps}
+						newsItemData={newsItemData}
+						hideFooterSettings={hideFooterSettings}
+						detailsType='content'
+					/>
+				)}
+			</section>
+		);
+	}
+);
 
 const NewsItemParent = (props) => {
 	const { state: userState } = useContext(UserContext);
