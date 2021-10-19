0ea4a0f53fec6e7b53ec225f4e250e2e2bb3e17e
DreamEcho100 <maze6572198@gmail.com>
2021-10-19 13:49:57 +0200

    - Changing intro styles.

--- components/Layout/MainIntro/index.jsx
+++ components/Layout/MainIntro/index.jsx
@@ -7,32 +7,49 @@ import LogNMazeSignature from '@svg/LogNMazeSignature';
 const MainIntro = () => {
 	// const [animationEnd, setAnimationEnd] = useState(false);
 	const [isWindowVertical, setIsWindowVertical] = useState(
-		typeof window !== 'undefined' && window.innerWidth > window.innerHeight ? false : true);
+		true ||
+			(typeof window !== 'undefined' && window.innerHeight > window.innerWidth)
+			? true
+			: false
+	);
+	const [isStartingIntro, setIsStartingIntro] = useState(false);
 
 	useEffect(() => {
 		const onResize = () => {
 			const width = window.innerWidth;
 			const height = window.innerHeight;
 
-			if (!isWindowVertical && width < height) setIsWindowVertical(true);
-			if (isWindowVertical && width > height) setIsWindowVertical(false);
+			console.log('width', width);
+			console.log('height', height);
+			console.log('isWindowVertical', isWindowVertical);
+
+			if (!isWindowVertical && width < height) return setIsWindowVertical(true);
+			if (isWindowVertical && width > height) return setIsWindowVertical(false);
 		};
 
 		onResize();
 
-		document.addEventListener('resize', onResize);
+		document.body.addEventListener('resize', onResize);
 
-		return () => document.removeEventListener('resize', onResize);
+		setIsStartingIntro(true);
+
+		return () => document.body.removeEventListener('resize', onResize);
 	}, []);
 
 	return (
 		<section
 			className={`${classes['main-intro']} ${
-				isWindowVertical && classes['window-is-vertical']
-			}`}
+				isWindowVertical ? classes['window-is-vertical'] : ''
+			}
+				${isStartingIntro ? classes['remove-intro'] : ''}
+			`}
 		>
 			<div className={classes.container}>
-				<div className={classes['svg-container']}>
+				<div
+					className={`${classes['svg-container']} ${
+						isStartingIntro ? classes['draw-signature'] : ''
+					}`}
+				>
 					<LogNMazeSignature
 					// setAnimationEnd={setAnimationEnd}
 					/>
