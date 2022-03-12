9e2ee9470a037bb82c2cfe9af0347d738c08cfcc
DreamEcho100 <maze6572198@gmail.com>
2022-03-13 01:15:44 +0200

    Test

--- components/_app/ExtraScripts/index.jsx
+++ components/_app/ExtraScripts/index.jsx
@@ -32,13 +32,13 @@ const ExtraScripts = () => {
       // data-checked-head={false}
     /> */}
 			<Script
-				// strategy='afterInteractive'
-				strategy='worker'
+				strategy='afterInteractive'
+				// strategy='worker'
 				src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
 			/>
 			<Script
-				// strategy='afterInteractive'
-				strategy='worker'
+				strategy='afterInteractive'
+				// strategy='worker'
 				id='google-analytics-script'
 				dangerouslySetInnerHTML={{
 					__html: `
