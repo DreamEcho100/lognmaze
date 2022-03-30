566ece705a8e2655cfa831fd6d18e2b44f6571f0
DreamEcho100 <maze6572198@gmail.com>
2022-03-30 22:55:41 +0200

    minor update.

--- screens/Users/Profile/index.tsx
+++ screens/Users/Profile/index.tsx
@@ -1,4 +1,5 @@
 import { useEffect } from 'react';
+import { NextSeo } from 'next-seo';
 
 import classes from './index.module.css';
 import helpersClasses from '@styles/helpers.module.css';
@@ -61,33 +62,73 @@ const UserProfileScreen = (Props: Props) => {
 	}
 
 	return (
-		<main className={helpersClasses.main}>
-			<SectionWrapper className={classes.userProfileHeroSectionWrapper}>
-				<UserProfileHero />
-			</SectionWrapper>
-			<div className={classes.mainContent}>
-				<NewsFeed
-				// newsFetchRouteQuery={{}}
-				// priorityForHeaderImage={false}
-				/>
-				<div>
-					<SectionWrapper className={classes.sectionWrapper}>
-						<ProfileBioSection bio={profilePageUserData.bio} />
-					</SectionWrapper>
-					{userData?.id && (
+		<>
+			<NextSeo
+				title={`${profilePageUserData.user_name_id} | LogNMaze | ${profilePageUserData.first_name} ${profilePageUserData.last_name}`}
+				description={profilePageUserData.bio || 'Bio is not provided yet!'}
+				canonical={`https://lognmaze.com/users/${profilePageUserData.user_name_id}`}
+				openGraph={{
+					title: `${profilePageUserData.user_name_id} | LogNMaze | ${profilePageUserData.first_name} ${profilePageUserData.last_name}`,
+					description: profilePageUserData.bio || 'Bio is not provided yet!',
+					url: `https://lognmaze.com/users/${profilePageUserData.user_name_id}`,
+					type: 'profile',
+					profile: {
+						firstName: profilePageUserData.first_name,
+						lastName: profilePageUserData.last_name,
+						username: profilePageUserData.user_name_id,
+						gender: profilePageUserData.gender,
+					},
+					...(() => {
+						const images: any[] = [];
+
+						if (profilePageUserData.profile_picture)
+							images.push({
+								url: profilePageUserData.profile_picture,
+								width: 850,
+								height: 650,
+								alt: 'Profile Picture',
+							});
+
+						if (profilePageUserData.cover_photo)
+							images.push({
+								url: profilePageUserData.cover_photo,
+								width: 850,
+								height: 650,
+								alt: 'Cover Photo',
+							});
+
+						return { images };
+					})(),
+				}}
+			/>
+			<main className={helpersClasses.main}>
+				<SectionWrapper className={classes.userProfileHeroSectionWrapper}>
+					<UserProfileHero />
+				</SectionWrapper>
+				<div className={classes.mainContent}>
+					<NewsFeed
+					// newsFetchRouteQuery={{}}
+					// priorityForHeaderImage={false}
+					/>
+					<div>
 						<SectionWrapper className={classes.sectionWrapper}>
-							<NewsItemActionButton
-								userToken={userToken}
-								actionType={'create'}
-								// newsItemData={undefined}
-								// newsItemDataType={'blog'}
-								userData={userData}
-							/>
+							<ProfileBioSection bio={profilePageUserData.bio} />
 						</SectionWrapper>
-					)}
+						{userData?.id && (
+							<SectionWrapper className={classes.sectionWrapper}>
+								<NewsItemActionButton
+									userToken={userToken}
+									actionType={'create'}
+									// newsItemData={undefined}
+									// newsItemDataType={'blog'}
+									userData={userData}
+								/>
+							</SectionWrapper>
+						)}
+					</div>
 				</div>
-			</div>
-		</main>
+			</main>
+		</>
 	);
 };
 
