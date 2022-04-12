9ceb0d898a79092e43429a5d680410a201b7b4a4
DreamEcho100 <maze6572198@gmail.com>
2022-04-12 23:52:44 +0200

    minor update.

--- components/core/_app/ExtraScripts/index.tsx
+++ components/core/_app/ExtraScripts/index.tsx
@@ -3,6 +3,7 @@ import Script from 'next/script';
 import { useRouter } from 'next/router';
 import * as gtag from '@commonLibIndependent/gtag';
 import pAddScript from './pAddScript';
+import CustomNextImage from '@commonComponentsDependent/CustomNextImage';
 
 const ExtraScripts = () => {
 	const router = useRouter();
@@ -37,13 +38,76 @@ const ExtraScripts = () => {
 				// strategy='worker'
 				src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
 			/>
+
 			<Script
-				strategy='afterInteractive'
+				strategy='worker'
+				id='googletagmanager'
 				// strategy='worker'
+				src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+			/>
+			<Script
+				strategy='worker'
+				id='gogleanalytics'
+				dangerouslySetInnerHTML={{
+					__html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+				}}
+			/>
+			<Script
+				// strategy='afterInteractive'
+				strategy='worker'
 				id='google-analytics-script'
 				src='https://stootsou.net/pfe/current/tag.min.js?z=4990432'
 				async
 			/>
+			{/* <!-- Yandex.Metrika counter --> */}
+			<Script
+				strategy='worker'
+				id='YandexMetrika'
+				dangerouslySetInnerHTML={{
+					__html: `   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+					m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+					(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+			 
+					ym(85004128, "init", {
+							 clickmap:true,
+							 trackLinks:true,
+							 accurateTrackBounce:true,
+							 webvisor:true
+					});`,
+				}}
+			/>
+			<Script
+				id='MicrosoftClarity'
+				strategy='worker'
+				dangerouslySetInnerHTML={{
+					__html: `    (function(c,l,a,r,i,t,y){
+						c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+						t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+						y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+				})(window, document, "clarity", "script", "8co4gbipfa");`,
+				}}
+			/>
+
+			<noscript>
+				<div>
+					<CustomNextImage
+						src='https://mc.yandex.ru/watch/85004128'
+						style={{
+							position: 'absolute',
+							left: '-9999px',
+						}}
+						alt=''
+					/>
+				</div>
+			</noscript>
+			{/* <!-- /Yandex.Metrika counter --> */}
 			<Script
 				id='superior-tag'
 				strategy='afterInteractive'
@@ -51,7 +115,6 @@ const ExtraScripts = () => {
 				data-cfasync='false'
 				async
 			/>
-			<script></script>
 			<Script
 				strategy='afterInteractive'
 				// strategy='worker'
--- libs/common/independent/gtag/index.ts
+++ libs/common/independent/gtag/index.ts
@@ -2,28 +2,38 @@ export const GA_TRACKING_ID = 'G-GYB2QBYV0D'; // process.env.NEXT_PUBLIC_GA_ID;
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url: string) => {
-	if (window.gtag)
-		window.gtag('config', GA_TRACKING_ID, {
-			page_path: url,
-		});
+	// if (window.gtag)
+	window.gtag('config', GA_TRACKING_ID, {
+		page_path: url,
+	});
 };
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/events
+// export const event = ({
+// 	action,
+// 	category,
+// 	label,
+// 	value,
+// }: {
+// 	action: string;
+// 	category: string;
+// 	label: string;
+// 	value: string;
+// }) => {
+// 	// if (window.gtag)
+// 	window.gtag('event', action, {
+// 		event_category: category,
+// 		event_label: label,
+// 		value: value,
+// 	});
+// };
 export const event = ({
 	action,
-	category,
-	label,
-	value,
+	params,
 }: {
 	action: string;
-	category: string;
-	label: string;
-	value: string;
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	params: { [key: string]: any };
 }) => {
-	if (window.gtag)
-		window.gtag('event', action, {
-			event_category: category,
-			event_label: label,
-			value: value,
-		});
+	window.gtag('event', action, params);
 };
