5ea400b47bc5db4e7defb7deab010e2b0284d347
DreamEcho100 <maze6572198@gmail.com>
2022-04-30 22:41:39 +0200

    Minor changes & bug fixes

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,7 @@ class MyDocument extends Document {
 		return (
 			<Html lang='en'>
 				<Head />
-				<body>
+				<body className='apply-basic-variables'>
 					<Main />
 					<NextScript />
 				</body>
