7073f21b1470314329cb2fb4d78d8394e6d55376
DreamEcho100 <maze6572198@gmail.com>
2023-01-04 15:02:02 +0200

    Minor update

--- src/components/shared/common/GoogleAdSense/HResponsiveImageV1.tsx
+++ src/components/shared/common/GoogleAdSense/HResponsiveImageV1.tsx
@@ -1,54 +1,66 @@
 import { useEffect, useRef } from 'react';
 
+const initConfigRef = () => ({
+	retryCounter: 0,
+	maxRetries: 7,
+	currentRetryInterval: 4000,
+	retryIntervalArr: [4000, 3000, 5000, 7000, 10000]
+});
+
 const GoogleAdSenseHResponsiveImageV1 = () => {
-	const configRef = useRef({
-		retryCounter: 0,
-		maxRetries: 7,
-		currentRetryInterval: 1000,
-		retryIntervalArr: [4000, 3000, 5000, 7000, 1000]
-	});
+	const configRef = useRef(initConfigRef());
 	useEffect(() => {
+		if (process.env.NODE_ENV !== 'production') return;
+
 		const pushAd = () => {
 			try {
+				if (!(window as any).adsbygoogle)
+					throw new Error("Can't find adsbygoogle");
 				const adsbygoogle = (window as any).adsbygoogle;
 				console.log({ adsbygoogle });
 				adsbygoogle.push({});
+				return true;
 			} catch (e) {
 				console.error(e);
+
+				configRef.current.retryCounter++;
+
+				configRef.current.currentRetryInterval =
+					configRef.current.retryIntervalArr[configRef.current.retryCounter] ||
+					Math.ceil(configRef.current.currentRetryInterval * 1.5);
+
+				return false;
 			}
 		};
 
-		configRef.current.currentRetryInterval =
-			configRef.current.retryIntervalArr[configRef.current.retryCounter] ||
-			Math.ceil(configRef.current.currentRetryInterval * 1.5);
+		if (pushAd()) return;
 
-		const currentRetryInterval = configRef.current.currentRetryInterval;
+		let rePushTimeoutId: NodeJS.Timeout;
 
-		const interval = setInterval(() => {
-			// Check if Adsense script is loaded every 300ms
-			if ((window as any).adsbygoogle) {
-				pushAd();
-				configRef.current.retryCounter++;
-				// clear the interval once the ad is pushed so that function isn't called indefinitely
-				clearInterval(interval);
-			}
+		const rePushTimeout = () => {
+			clearTimeout(rePushTimeoutId);
 
-			if (configRef.current) clearInterval(interval);
-		}, currentRetryInterval);
+			rePushTimeoutId = setTimeout(() => {
+				if (
+					configRef.current.maxRetries === configRef.current.retryCounter ||
+					pushAd()
+				)
+					return;
+
+				rePushTimeout();
+			}, configRef.current.currentRetryInterval);
+		};
+
+		rePushTimeout();
 
 		return () => {
-			clearInterval(interval);
+			clearTimeout(rePushTimeoutId);
 		};
 	}, []);
 
 	useEffect(() => {
 		return () => {
-			configRef.current = {
-				retryCounter: 0,
-				maxRetries: 7,
-				currentRetryInterval: 1000,
-				retryIntervalArr: [1000, 3000, 5000, 7000, 1000]
-			};
+			configRef.current = initConfigRef();
 		};
 	}, []);
 
