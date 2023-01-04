87840a94612d00d5472059ef191feb19bb96069e
DreamEcho100 <maze6572198@gmail.com>
2023-01-04 13:45:02 +0200

    Minor update

--- src/components/shared/common/GoogleAdSense/HResponsiveImageV1.tsx
+++ src/components/shared/common/GoogleAdSense/HResponsiveImageV1.tsx
@@ -1,6 +1,12 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 
 const GoogleAdSenseHResponsiveImageV1 = () => {
+	const configRef = useRef({
+		retryCounter: 0,
+		maxRetries: 7,
+		currentRetryInterval: 1000,
+		retryIntervalArr: [1000, 3000, 5000, 7000, 1000]
+	});
 	useEffect(() => {
 		const pushAd = () => {
 			try {
@@ -12,14 +18,23 @@ const GoogleAdSenseHResponsiveImageV1 = () => {
 			}
 		};
 
+		configRef.current.currentRetryInterval =
+			configRef.current.retryIntervalArr[configRef.current.retryCounter] ||
+			Math.ceil(configRef.current.currentRetryInterval * 1.5);
+
+		const currentRetryInterval = configRef.current.currentRetryInterval;
+
 		const interval = setInterval(() => {
 			// Check if Adsense script is loaded every 300ms
 			if ((window as any).adsbygoogle) {
 				pushAd();
+				configRef.current.retryCounter++;
 				// clear the interval once the ad is pushed so that function isn't called indefinitely
 				clearInterval(interval);
 			}
-		}, 300);
+
+			if (configRef.current) clearInterval(interval);
+		}, currentRetryInterval);
 
 		return () => {
 			clearInterval(interval);
@@ -36,14 +51,6 @@ const GoogleAdSenseHResponsiveImageV1 = () => {
 			data-full-width-responsive='true'
 		></ins>
 	);
-	/*
-	<Script
-		id=''
-		dangerouslySetInnerHTML={{
-			__html: `(adsbygoogle = window.adsbygoogle || []).push({});`
-		}}
-/>
-	*/
 };
 
 export default GoogleAdSenseHResponsiveImageV1;
