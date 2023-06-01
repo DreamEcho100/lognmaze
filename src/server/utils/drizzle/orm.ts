0a73172e3724d2e12734b9ab13358788defacc97
DreamEcho100 <maze6572198@gmail.com>
2023-05-31 17:48:57 -0700

    Minor update

--- src/server/utils/drizzle/orm.ts
+++ src/server/utils/drizzle/orm.ts
@@ -4,8 +4,6 @@ import { Pool } from 'pg';
 import { env } from 'process';
 import drizzleSchema from '@server/utils/drizzle/schema';
 
-console.log('\n\n\n', env.DATABASE_URL, '\n\n\n');
-
 const pool = new Pool({
 	connectionString: env.DATABASE_URL
 	// connectionString: `${env.DATABASE_URL}?sslmode=require`
