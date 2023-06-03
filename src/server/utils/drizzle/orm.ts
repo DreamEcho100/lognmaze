5c29e5f6c8cbf6dd218606ff0e2c3b583016900d
DreamEcho100 <maze6572198@gmail.com>
2023-06-02 17:44:09 -0700

    Minor update

--- src/server/utils/drizzle/orm.ts
+++ src/server/utils/drizzle/orm.ts
@@ -1,6 +1,7 @@
 import { drizzle } from 'drizzle-orm/neon-serverless';
-// import { Pool } from '@neondatabase/serverless';
-import { Pool } from 'pg';
+import { Pool } from '@neondatabase/serverless';
+// import postgres from 'postgres';
+// import { Pool } from 'pg';
 import { env } from 'process';
 import drizzleSchema from '@server/utils/drizzle/schema';
 
@@ -8,7 +9,9 @@ const pool = new Pool({
 	connectionString: env.DATABASE_URL
 	// connectionString: `${env.DATABASE_URL}?sslmode=require`
 });
+// postgres(`${process.env.DATABASE_URL}`, { ssl: 'require', max: 1 })
+
 export const drizzleClient = drizzle(pool, {
 	schema: drizzleSchema,
-	logger: env.NODE_ENV === 'development'
+	logger: env.NODE_ENV === 'development',
 });
