17cb3d0e165a5154cac361648b5873cf06c3a460
DreamEcho100 <maze6572198@gmail.com>
2023-06-05 14:09:33 -0700

    Minor update

--- src/server/utils/drizzle/orm.ts
+++ src/server/utils/drizzle/orm.ts
@@ -1,8 +1,11 @@
 import { drizzle } from 'drizzle-orm/neon-serverless';
-// import { Pool } from '@neondatabase/serverless';
-import { Pool } from 'pg';
+import { neonConfig, Pool } from '@neondatabase/serverless';
+import ws from 'ws';
+// import { Pool } from 'pg';
 import { env } from 'process';
 import drizzleSchema from '@server/utils/drizzle/schema';
+
+neonConfig.webSocketConstructor = ws;
 
 const pool = new Pool({
 	connectionString: env.DATABASE_URL
